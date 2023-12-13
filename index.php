b8447c2c6c76a0b5e167cee94495fc382534777a
FedericoPiazzolla <Federicop257@gmail.com>
2023-12-13 17:49:58 +0100

    add: form in index.php whitout control of email

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+
+  <!-- Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+  <main>
+    <div class="container my-5">
+
+    <h1 class="text-center">Perfavore Inserisca la sua Mail</h1>
+
+    <form class="form-control align-items-center d-flex flex-column" action="">
+
+    <label class="form-label" for="mail">Email Address</label>
+    <input class="form-control" type="text" id="mail" name="mail" placeholder="name@gmail.com">
+
+    <button class="btn btn-primary mt-4" type="submit">LOGIN</button>
+    </form>
+    </div>
+  </main>
+</body>
+
+</html>
