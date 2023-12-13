8484ae47ba596ec442f4fdb845d25ed217b18222
FedericoPiazzolla <Federicop257@gmail.com>
2023-12-13 18:10:23 +0100

    add: control of email with function and print on page result

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
+<?php 
+  function email_control($email) {
+    if (isset($email)) {
+
+      if (!str_contains($email, '@') || !str_contains($email, '.')) {
+        return false;
+      } else {
+        return true;
+      };
+    };
+  };
+
+  $email_convalidation = email_control($_POST['email']);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>NewsLetter</title>
 
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -16,13 +31,19 @@
 
     <h1 class="text-center">Perfavore Inserisca la sua Mail</h1>
 
-    <form class="form-control align-items-center d-flex flex-column" action="">
+    <!-- Email form -->
+    <form class="form-control align-items-center d-flex flex-column" action="index.php" method="POST">
 
-    <label class="form-label" for="mail">Email Address</label>
-    <input class="form-control" type="text" id="mail" name="mail" placeholder="name@gmail.com">
+    <label class="form-label" for="email">Email Address</label>
+    <input class="form-control" type="text" id="email" name="email" placeholder="name@gmail.com">
 
     <button class="btn btn-primary mt-4" type="submit">LOGIN</button>
     </form>
+    <!-- /Email Form -->
+
+    <h2 class="text-center mt-5 alert <?= $email_convalidation ? 'alert-success' : 'alert-danger' ?>">
+      <?= $email_convalidation ? 'Indirizzo Mail valido!!' : 'Indirizzo Email NON valido' ?>
+    </h2>
     </div>
   </main>
 </body>
